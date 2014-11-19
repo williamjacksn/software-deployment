fa7e52513c764a3edbef057b96cb13a98de5152e
William Jackson <william@subtlecoolness.com>
2014-11-19 10:41:19 -0600

    Update Flash Player

--- flash_player_activex.bat
+++ flash_player_activex.bat
@@ -1,16 +1,20 @@
+eventcreate /l application /t information /so SCCM /id 5 /d "%~dpnx0 %*"
+set app_name=Flash Player ActiveX
+set app_file_name=flash_player_activex
+
 goto [%1]
 goto []
 
 :[install]
-eventcreate /l application /t information /so SCCM /id 1 /d "Beginning Flash Player ActiveX install..."
-for %%a in ("%~dp0*.msi") do msiexec.exe /package "%%a" /quiet /norestart REMOVE_PREVIOUS=YES
-eventcreate /l application /t information /so SCCM /id 2 /d "Ending Flash Player ActiveX install..."
+eventcreate /l application /t information /so SCCM /id 1 /d "Beginning %app_name% install"
+for %%a in ("%~dp0*.msi") do msiexec.exe /package "%%a" /quiet /norestart /l*v "%windir%\Temp\_%app_file_name%_install.log" REMOVE_PREVIOUS=YES
+eventcreate /l application /t information /so SCCM /id 2 /d "Ending %app_name% install"
 goto []
 
 :[uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning Flash Player ActiveX uninstall..."
-for %%a in ("%~dp0*.msi") do msiexec.exe /uninstall "%%a" /quiet /norestart
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending Flash Player ActiveX uninstall..."
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall"
+for %%a in ("%~dp0*.msi") do msiexec.exe /uninstall "%%a" /quiet /norestart /l*v "%windir%\Temp\_%app_file_name%_uninstall.log"
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall"
 goto []
 
 :[]
