04bc1318934e8bb39c0e1bb270393ff6bad0fc53
William Jackson <william@subtlecoolness.com>
2014-11-19 10:41:03 -0600

    Update Firefox

--- firefox.bat
+++ firefox.bat
@@ -1,23 +1,27 @@
+eventcreate /l application /t information /so SCCM /id 5 /d "%~dpnx0 %*"
+set app_name=Firefox
+set app_file_name=firefox
+
 goto [%PROCESSOR_ARCHITECTURE%-%1]
 goto []
 
 :[x86-install]
 :[AMD64-install]
-eventcreate /l application /t information /so SCCM /id 1 /d "Beginning Firefox install"
+eventcreate /l application /t information /so SCCM /id 1 /d "Beginning %app_name% install"
 for %%a in ("%~dp0*.exe") do "%%a" /ini="%~dp0firefox.ini"
-eventcreate /l application /t information /so SCCM /id 2 /d "Ending Firefox install"
+eventcreate /l application /t information /so SCCM /id 2 /d "Ending %app_name% install"
 goto []
 
 :[x86-uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning Firefox uninstall (32-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall (32-bit Windows)"
 "%ProgramFiles%\Mozilla Firefox\uninstall\helper.exe" /S
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending Firefox uninstall (32-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall (32-bit Windows)"
 goto []
 
 :[AMD64-uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning Firefox uninstall (64-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall (64-bit Windows)"
 "%ProgramFiles(x86)%\Mozilla Firefox\uninstall\helper.exe" /S
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending Firefox uninstall (64-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall (64-bit Windows)"
 goto []
 
 :[]
