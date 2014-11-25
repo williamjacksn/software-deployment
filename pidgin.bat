6923dda5a121a2b4ea7db33f7b7713ef50d55f55
William Jackson <william@subtlecoolness.com>
2014-11-25 12:42:53 -0600

    Update Pidgin

--- pidgin.bat
+++ pidgin.bat
@@ -1,23 +1,27 @@
+eventcreate /l application /t information /so SCCM /id 5 /d "%~dpnx0 %*"
+set app_name=Pidgin
+set app_file_name=pidgin
+
 goto [%PROCESSOR_ARCHITECTURE%-%1]
 goto []
 
 :[x86-install]
 :[AMD64-install]
-eventcreate /l application /t information /so SCCM /id 1 /d "Beginning Pidgin install"
+eventcreate /l application /t information /so SCCM /id 1 /d "Beginning %app_name% install"
 for %%a in ("%~dp0*.exe") do "%%a" /S
-eventcreate /l application /t information /so SCCM /id 2 /d "Ending Pidgin install"
+eventcreate /l application /t information /so SCCM /id 2 /d "Ending %app_name% install"
 goto []
 
 :[x86-uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning Pidgin uninstall (32-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall (32-bit Windows)"
 "%ProgramFiles%\Pidgin\pidgin-uninst.exe" /S
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending Pidgin uninstall (32-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall (32-bit Windows)"
 goto []
 
 :[AMD64-uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning Pidgin uninstall (64-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall (64-bit Windows)"
 "%ProgramFiles(x86)%\Pidgin\pidgin-uninst.exe" /S
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending Pidgin uninstall (64-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall (64-bit Windows)"
 goto []
 
 :[]
