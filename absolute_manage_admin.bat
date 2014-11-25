bc40dbf722e9e2c13bf5d56e4491ce4a48916a9f
William Jackson <william@subtlecoolness.com>
2014-11-25 12:03:43 -0600

    Update Absolute Manage Agent and Admin

--- absolute_manage_admin.bat
+++ absolute_manage_admin.bat
@@ -1,16 +1,20 @@
+eventcreate /l application /t information /so SCCM /id 5 /d "%~dpnx0 %*"
+set app_name=Absolute Manage Admin
+set app_file_name=absolute_manage_admin
+
 goto [%1]
 goto []
 
 :[install]
-eventcreate /l application /t information /so SCCM /id 1 /d "Beginning Absolute Manage Admin install..."
-"%~dp0setup.exe" /s /v"/quiet /norestart"
-eventcreate /l application /t information /so SCCM /id 2 /d "Ending Absolute Manage Admin install..."
+eventcreate /l application /t information /so SCCM /id 1 /d "Beginning %app_name% install"
+"%~dp0setup.exe" /s /v"/quiet /norestart /l*v %windir%\Temp\_%app_file_name%_install.log"
+eventcreate /l application /t information /so SCCM /id 2 /d "Ending %app_name% install"
 goto []
 
 :[uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning Absolute Manage Admin uninstall..."
-msiexec.exe /uninstall "%~dp0LANrev Admin.msi" /quiet /norestart
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending Absolute Manage Admin uninstall..."
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall"
+msiexec.exe /uninstall "%~dp0LANrev Admin.msi" /quiet /norestart /l*v "%windir%\Temp\_%app_file_name%_uninstall.log"
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall"
 goto []
 
 :[]
--- absolute_manage_agent.bat
+++ absolute_manage_agent.bat
@@ -1,17 +1,21 @@
+eventcreate /l application /t information /so SCCM /id 5 /d "%~dpnx0 %*"
+set app_name=Absolute Manage Agent
+set app_file_name=absolute_manage_agent
+
 goto [%1]
 goto []
 
 :[install]
-eventcreate /l application /t information /so SCCM /id 1 /d "Beginning Absolute Manage Agent install..."
-"%~dp0setup.exe" /s /v"/quiet /norestart /l*vx C:\Windows\Temp\_absolute_manage_agent_install.log"
+eventcreate /l application /t information /so SCCM /id 1 /d "Beginning %app_name% install"
+"%~dp0setup.exe" /s /v"/quiet /norestart /l*v %windir%\Temp\_%app_file_name%_install.log"
 ping -n 60 localhost > nul
-eventcreate /l application /t information /so SCCM /id 2 /d "Ending Absolute Manage Agent install..."
+eventcreate /l application /t information /so SCCM /id 2 /d "Ending %app_name% install"
 goto []
 
 :[uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning Absolute Manage Agent uninstall..."
-msiexec.exe /uninstall "%~dp0LANrev Agent.msi" /quiet /norestart /l*vx C:\Windows\Temp\_absolute_manage_agent_uninstall.log
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending Absolute Manage Agent uninstall..."
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall"
+msiexec.exe /uninstall "%~dp0LANrev Agent.msi" /quiet /norestart /l*v %windir%\Temp\_%app_file_name%_uninstall.log
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall"
 goto []
 
 :[]
