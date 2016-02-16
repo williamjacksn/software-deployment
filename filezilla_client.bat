10fd1b89762c9d5a15751604aafbf98da64ae257
William Jackson <william@subtlecoolness.com>
2016-02-16 09:48:02 -0600

    Update FileZilla Client

--- filezilla_client.bat
+++ filezilla_client.bat
@@ -2,26 +2,19 @@ eventcreate /l application /t information /so SCCM /id 5 /d "%~dpnx0 %*"
 set app_name=FileZilla Client
 set app_file_name=filezilla_client
 
-goto [%PROCESSOR_ARCHITECTURE%-%1]
+goto [%1]
 goto []
 
-:[x86-install]
-:[AMD64-install]
+:[install]
 eventcreate /l application /t information /so SCCM /id 1 /d "Beginning %app_name% install"
-for %%a in ("%~dp0*.exe") do "%%a" /S
+for %%a in ("%~dp0%PROCESSOR_ARCHITECTURE%\*.exe") do "%%a" /S
 eventcreate /l application /t information /so SCCM /id 2 /d "Ending %app_name% install"
 goto []
 
-:[x86-uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall (32-bit Windows)"
+:[uninstall]
+eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall"
 "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" /S
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall (32-bit Windows)"
-goto []
-
-:[AMD64-uninstall]
-eventcreate /l application /t information /so SCCM /id 3 /d "Beginning %app_name% uninstall (64-bit Windows)"
-"%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" /S
-eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall (64-bit Windows)"
+eventcreate /l application /t information /so SCCM /id 4 /d "Ending %app_name% uninstall"
 goto []
 
 :[]
